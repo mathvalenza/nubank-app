5fe1e66ae6f4b18c5b5df3f2718b2ed3cd34c257
Valenza <matheusvvalenza@gmail.com>
2020-03-27 14:36:42 -0300

    added main card

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,43 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
 
-import {Container} from './styles';
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  Title,
+  Description,
+  Annotation,
+} from './styles';
 
 export default function Main() {
   return (
     <Container>
       <Header />
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#667" />
+            <Icon name="visibilitty-off" size={28} color="#667" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 997.122,75</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Trasnferência de R$20,00 recebida de Matheus Vinícius Valenza hoje
+              às 12:10
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
       <Tabs />
     </Container>
   );
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -3,4 +3,54 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
   background: #8b10ae;
+  justify-content: center;
+`;
+
+export const Content = styled.View`
+  flex: 1;
+  max-height: 400px;
+  z-index: 5;
+`;
+
+export const Card = styled.View`
+  flex: 1;
+  background: #fff;
+  border-radius: 4px;
+  margin: 0 20px;
+  height: 100%;
+`;
+
+export const CardHeader = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 30px;
+`;
+
+export const CardContent = styled.View`
+  flex: 1;
+  padding: 0 30px;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  font-size: 13px;
+  color: #999;
+`;
+
+export const Description = styled.Text`
+  font-size: 32px;
+  margin-top: 3px;
+  color: #333;
+`;
+
+export const CardFooter = styled.View`
+  padding: 30px;
+  background: #eee;
+  border-radius: 4px;
+`;
+
+export const Annotation = styled.Text`
+  font-size: 12px;
+  color: #333;
 `;
