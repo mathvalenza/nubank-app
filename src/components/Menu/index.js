189d9a115cb4576d22b1946766618825f7777570
Valenza <matheusvvalenza@gmail.com>
2020-03-27 15:04:32 -0300

    added not broken qrcode lib

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import QRCode from 'react-native-qrcode-svg';
 
-import {Container} from './styles';
+import {Container, Code} from './styles';
 
 export default function Menu() {
-  return <Container />;
+  return (
+    <Container>
+      <Code>
+        <QRCode
+          value="http://github.com/mathvalenza"
+          size={80}
+          bgColor="#fff"
+          fgColor="#8B10AE"
+        />
+      </Code>
+    </Container>
+  );
 }
--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -1,3 +1,7 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View``;
+
+export const Code = styled.View`
+  overflow: hidden;
+`;
