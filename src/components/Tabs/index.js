0b856cd53e3d5c441fd8e12190dc1f22f249bda1
Valenza <matheusvvalenza@gmail.com>
2020-03-26 23:24:54 -0300

    added tab items

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
 
-import {Container} from './styles';
+import {Container, TabsContainer, TabItem, TabText} from './styles';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default function Tabs() {
-  return <Container />;
+  return (
+    <Container>
+      <TabsContainer>
+        <TabItem>
+          <Icon name="person-add" size={24} color="#fff" />
+          <TabText>Indicar amigos</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="chat-bubble-outline" size={24} color="#fff" />
+          <TabText>Cobrar</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="arrow-downward" size={24} color="#fff" />
+          <TabText>Depositar</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="arrow-upward" size={24} color="#fff" />
+          <TabText>Transferir</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="lock" size={24} color="#fff" />
+          <TabText>Bloquear cartão</TabText>
+        </TabItem>
+      </TabsContainer>
+    </Container>
+  );
 }
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Header from '~/components/Header';
+import Tabs from '~/components/Tabs';
 
 import {Container} from './styles';
 
@@ -8,6 +9,7 @@ export default function Main() {
   return (
     <Container>
       <Header />
+      <Tabs />
     </Container>
   );
 }
