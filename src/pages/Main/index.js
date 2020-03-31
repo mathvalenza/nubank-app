e29932d6902eb5fb2c743f81f161040ec13fa800
Valenza <matheusvvalenza@gmail.com>
2020-03-30 22:20:01 -0300

    finished card animation

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -21,6 +21,7 @@ import {
 } from './styles';
 
 export default function Main() {
+  let offset = 0;
   const translateY = new Animated.Value(0);
 
   const animatedEvent = Animated.event(
@@ -34,7 +35,32 @@ export default function Main() {
     {useNativeDriver: true},
   );
 
-  function onHandlerStateChange(event) {}
+  function onHandlerStateChange(event) {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      let opened = false;
+      const {translationY} = event.nativeEvent;
+
+      offset += translationY;
+
+      if (translationY >= 100) {
+        opened = true;
+      } else {
+        translateY.setValue(offset);
+        translateY.setOffset(0);
+        offset = 0;
+      }
+
+      Animated.timing(translateY, {
+        toValue: opened ? 380 : 0,
+        duration: 400,
+        useNativeDriver: true,
+      }).start(() => {
+        offset = opened ? 380 : 0;
+        translateY.setOffset(offset);
+        translateY.setValue(0);
+      });
+    }
+  }
 
   return (
     <Container>
